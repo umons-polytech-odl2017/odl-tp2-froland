628042d38ef12b6e96a246eda1eeb9a53485f531
François Roland <francois.roland@umons.ac.be>
2017-10-27 14:18:14 +0200

    Write exercise 3 solution

---
+++ src/main/java/exercise3/ConvexPolygon.java
@@ -0,0 +1,15 @@
+package exercise3;
+
+public class ConvexPolygon implements Drawable {
+
+	private final Point[] points;
+
+	public ConvexPolygon(Point[] points) {
+		this.points = points;
+	}
+
+	@Override
+	public Point[] getVertices() {
+		return points;
+	}
+}
--- src/main/java/exercise3/Exercise3.java
+++ src/main/java/exercise3/Exercise3.java
@@ -2,6 +2,12 @@ package exercise3;
 
 public class Exercise3 {
 	public static void main(String[] args) {
-		new Panel(new Drawable[0]);
+		new Panel(new Drawable[] {
+			new ConvexPolygon(new Point[]{
+				new Point(1, 1),
+				new Point(100, 100),
+				new Point(200, 1)
+			})
+		});
 	}
 }
